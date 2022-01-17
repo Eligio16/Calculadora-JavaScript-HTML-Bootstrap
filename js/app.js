a07c5d7d807fd9ec6c906022fa4d8fe563fbd047
Eligio Quijada <eligio.eq@gmail.com>
2022-01-17 10:45:26 -0500

    Se añade funciones de resta, multiplicación, división y estilos

--- js/app.js
+++ js/app.js
@@ -9,5 +9,47 @@ function sumar(){
         resultado = 'La operacion no incluye numeros';
     }
     document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
-    console.log(`Resultado: ${resultado}`)
+    console.log(`Resultado: ${resultado}`);
 }
+
+function restar(){
+    const forma = document.getElementById('forma');
+    let operandoA = forma['operandoA'];
+    let operandoB = forma['operandoB'];
+    let resultado = parseInt(operandoA.value) - parseInt(operandoB.value);
+    if (isNaN(resultado)){
+        resultado = 'La operacion no incluye numeros';
+    }
+    document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
+    console.log(`Resultado: ${resultado}`);
+}
+
+function multiplicar(){
+    const forma = document.getElementById('forma');
+    let operandoA = forma['operandoA'];
+    let operandoB = forma['operandoB'];
+    let resultado = parseInt(operandoA.value) * parseInt(operandoB.value);
+    if (isNaN(resultado)){
+        resultado = 'La operacion no incluye numeros';
+    }
+    document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
+    console.log(`Resultado: ${resultado}`);
+}
+function dividir(){
+    const forma = document.getElementById('forma');
+    let operandoA = forma['operandoA'];
+    let operandoB = forma['operandoB'];
+    let resultado = parseInt(operandoA.value) / parseInt(operandoB.value);
+    if (isNaN(resultado)){
+        resultado = 'La operacion no incluye numeros';
+    }
+    document.getElementById('resultado').innerHTML = `Resultado: ${resultado}`;
+    console.log(`Resultado: ${resultado}`);
+}
+
+forma.addEventListener('focus', (evento) =>{
+    evento.target.style.background = '#E0E6EF'
+}, true);
+forma.addEventListener('blur', (evento) =>{
+    evento.target.style.background = 'white'
+}, true);
